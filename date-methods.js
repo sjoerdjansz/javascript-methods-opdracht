d849aca5c0bc787702ba9c04b2ffe7bcb058071d
sjoerdjansz <sjoerdjansz@gmail.com>
2025-04-05 16:19:20 +0200

    laatste opdracht (Dates) afgemaakt

--- date-methods.js
+++ date-methods.js
@@ -13,8 +13,22 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
+const today = new Date();
 
+console.log(`The time is: ${today.getHours()}:${today.getMinutes()}`);
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+const readableDateFormat = today.toDateString();
+
+const readableLocaleDateFormat = today.toLocaleDateString('nl-NL', {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+});
+
+console.log(readableDateFormat);
+console.log(readableLocaleDateFormat);
